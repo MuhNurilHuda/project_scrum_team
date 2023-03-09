7ffa73b7083502d6c5d9a369d9b71d7c40bb7614
Pstar7 <muhammadnurilhuda374@gmail.com>
2023-03-09 16:30:22 +0700

    Tuesday, 07-03-2023

--- lib/pages/paket_wisata.dart
+++ lib/pages/paket_wisata.dart
@@ -32,14 +32,6 @@ class PaketWisata extends StatelessWidget {
           },
           itemCount: tourismPlaceList.length,
         )
-      // body: InkWell(
-      //   onTap: () {
-      //     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      //       return DetailScreen();
-      //     }));
-      //   },
-      //   child: listItem(),
-      // ),
     );
   }
 
@@ -54,12 +46,6 @@ class PaketWisata extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20.0),
         child: InkWell(
-          // onTap: () {
-          //   Navigator.push(
-          //     context,
-          //     MaterialPageRoute(builder: (context) => )
-          //   )
-          // },
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
@@ -76,39 +62,6 @@ class PaketWisata extends StatelessWidget {
         ),
       ),
     );
-    // return Card(
-    //   child: Row(
-    //     crossAxisAlignment: CrossAxisAlignment.start,
-    //     children: <Widget>[
-    //       Expanded(
-    //         flex: 1,
-    //         child: Image.asset(place.imageAsset),
-    //       ),
-    //       Expanded(
-    //         flex: 2,
-    //         child: Padding(
-    //           padding: const EdgeInsets.all(8.0),
-    //           child: Column(
-    //             crossAxisAlignment: CrossAxisAlignment.start,
-    //             mainAxisSize: MainAxisSize.min,
-    //             children: <Widget>[
-    //               Text(
-    //                 place.name,
-    //                 style: TextStyle(
-    //                     fontSize: 16.0
-    //                 ),
-    //               ),
-    //               SizedBox(
-    //                 height: 10,
-    //               ),
-    //               Text(place.location)
-    //             ],
-    //           ),
-    //         ),
-    //       )
-    //     ],
-    //   ),
-    // );
   }
 }
 
