d4a78772744eb0b5f16cd2da6e52e6a1144c38d3
Pstar7 <muhammadnurilhuda374@gmail.com>
2023-03-10 08:12:05 +0700

    Tuesday, 07-03-2023

---
+++ lib/navigation/bottom_navbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BottomNavbar extends StatefulWidget {
+  const BottomNavbar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavbar> createState() => _BottomNavbarState();
+}
+
+class _BottomNavbarState extends State<BottomNavbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
